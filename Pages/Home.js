45c0f256500aaa3c0cf88598892290b4fd872c3e
Nina <ntemridiwong@ufl.edu>
2022-12-03 20:10:50 -0500

    Add button

--- Pages/Home.js
+++ Pages/Home.js
@@ -1,14 +1,30 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList, Button} from "react-native";
 import { TabRouter, useRoute } from "@react-navigation/native";
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 import Login from "./Login";
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Home() {
   const route = useRoute();
 
   const [groups, setGroups] = React.useState();
+
+  const userInfo = useSelector((state) => state.userInfo.value);
+
+  async function addUserToGroup(groupName) {
+      const addUser = await fetch(
+        `https://groupie-backend.herokuapp.com/addUserToGroup/${groupName}`, {
+          method: "POST",               
+          body: JSON.stringify({email: userInfo.payload.email}),               
+          headers: {                 
+            "Content-type": "application/json; charset=UTF-8",               
+          },
+        }
+      );
+      addUser.json().then((data)=>{console.log(data)})
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -17,7 +33,7 @@ export default function Home() {
       );
       groups.json().then((data) => {
         console.log(data);
-        setGroups(data);
+        setGroups(data.data);
       });
     }
     fetchData();
@@ -27,7 +43,22 @@ export default function Home() {
     if (route) {
       return (
         <View style={styles.container}>
-          <Text style={styles.title}>{"ji"}</Text>
+          <FlatList 
+              data={groups}
+              keyExtractor={(item, index) => { return item.groupID}}
+              renderItem={({item, index}) => (
+                <View>
+                  <Text style={styles.title}>{item.groupName}</Text>
+                  {item.tags.map((tag, i) => (
+                    <Text key={i}>{tag}</Text>
+                  ))}
+                  <Button onPress={()=>{addUserToGroup(item.groupName)}} title="Join">
+
+                  </Button>
+                  
+                </View>
+              )}
+          />
         </View>
       );
     } else {
